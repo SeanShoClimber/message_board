ccd591313faba836b331cc579bf09b94a0f5a0ab
Sean.Shin <gitboulder@gmail.com>
2024-07-04 08:52:08 +0900

    「getAllMessagesを追加」

--- src/models/Message.java
+++ src/models/Message.java
@@ -6,9 +6,19 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllMessages",
+        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+    )
+})
+
 @Table(name = "messages")
 public class Message {
     @javax.persistence.Id
